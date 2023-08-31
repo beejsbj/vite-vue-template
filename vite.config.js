58d1452f54c25a9791d40984843893e5ccb2428a
beejsbj <burooj.bj@gmail.com>
2023-08-30 20:34:46 -0400

    vite commit

--- vite.config.js
+++ vite.config.js
@@ -6,26 +6,43 @@ import eslint from "@rollup/plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          //every tag with '-' is a custom element
-          isCustomElement: (tag) => tag.includes("-"),
-        },
-      },
-    }),
-    {
-      ...eslint({
-        include: "src/**/*.*/js",
-      }),
-      enforce: "pre",
-      apply: "build",
-    },
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+	plugins: [
+		vue({
+			template: {
+				compilerOptions: {
+					//every tag with '-' is a custom element
+					isCustomElement: (tag) => tag.includes("-"),
+				},
+			},
+		}),
+		{
+			...eslint({
+				include: "src/**/*.*/js",
+			}),
+			enforce: "pre",
+			apply: "build",
+		},
+		AutoImport({
+			/* options */
+			imports: [
+				"vue",
+				"pinia",
+
+				{
+					// "@/stores/shop": ["useShopStore"],
+				},
+			],
+			dirs: ["src/stores"],
+			dts: true,
+		}),
+		Components({
+			dirs: ["src/views", "src/components", "src/views/pages"],
+			dts: true,
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
 });
